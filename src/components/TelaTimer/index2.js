10141850f8d965838a20f3d163414a06894175e5
Uitalo Souza <uitalo.urss@gmail.com>
2020-10-18 00:35:16 -0300

    ultimos ajustes do timer

--- src/components/TelaTimer/index2.js
+++ src/components/TelaTimer/index2.js
@@ -1,4 +1,7 @@
 import React, { Component } from 'react';
+import './style.css';
+import Botao from '../Botao/index3'
+
 
 class Temporizador extends Component{
     constructor(props){
@@ -38,14 +41,9 @@ class Temporizador extends Component{
         event.preventDefault();
     }
 
-     componentDidMount(){
+    componentDidMount(){
          this.myInterval = setInterval(() =>{
             const {horas, minutos, segundos} = this.state;
-            if(segundos > 0){
-                this.setState(({segundos}) =>({
-                    segundos: segundos - 1
-                }))
-            }
 
             if(horas !== 0 && minutos === 0 && segundos === 0){
                 this.setState(({horas}) => ({
@@ -55,11 +53,19 @@ class Temporizador extends Component{
                 }))
             }
 
-            if (minutos !== 0 && segundos === 0){
-                this.setState(({minutos}) => ({
-                    minutos: minutos -1,
-                    segundos: 59
-                }))
+            if (minutos <= 59){
+                if(segundos > 0 && segundos <= 59){
+                    this.setState(({segundos}) =>({
+                        segundos: segundos - 1
+                    }))
+                }
+
+                if (minutos !== 0 && segundos === 0){
+                    this.setState(({minutos}) => ({
+                        minutos: minutos -1,
+                        segundos: 59
+                    }))
+                }
             }
          }, 1000)
      }
@@ -70,26 +76,35 @@ class Temporizador extends Component{
 
     render(){
         const { horas, minutos, segundos } = this.state
+
+        var msg;
+        if(horas === undefined || minutos === undefined || segundos === undefined){
+            msg = ""
+        }else if(horas === 0 && minutos === 0 && segundos === 0){
+            msg = <h2 className="msgTimer">Fim da Contagem</h2>
+        }else{
+            msg = <h3 className="msgTimer">Tempo restante: {horas < 10? `0${horas}`: horas}:{minutos < 10? `0${minutos}`: minutos}:{segundos < 10? `0${segundos}`: segundos}</h3>
+        }
         return(
             <>
             <form onSubmit={this.handleSubmit}>
                 <div className="form-row">
                     <div className="form-group col-md-3">
                         <label for="inputCity">Horas</label>
-                        <input type="text" value={this.state.horas} onChange={this.handleHorasChange} className="form-control" />
+                        <input type="number" min="0" value={this.state.horas} onChange={this.handleHorasChange} className="form-control" placeholder="0" />
                     </div>
                     <div className="form-group col-md-3">
                         <label for="inputState">Minutos</label>
-                        <input type="text" value={this.state.minutos} onChange={this.handleMinutosChange} className="form-control" />
+                        <input type="number" maxLength="2" min="0" max="59" value={this.state.minutos} onChange={this.handleMinutosChange} className="form-control" placeholder="0"/>
                     </div>
                     <div className="form-group col-md-3">
                         <label for="inputZip">Segundos</label>
-                        <input type="text" value={this.state.segundos} onChange={this.handleSegundosChange} className="form-control" />
+                        <input type="number" maxLength="2" max="59" min="0" value={this.state.segundos} onChange={this.handleSegundosChange} className="form-control" placeholder="0"/>
                     </div>
                 </div>
             </form>
             <div>
-                {horas === 0 && minutos === 0 && segundos === 0 ? <h2>Acabou essa desgraça!</h2> :  <h3>Tempo restante: {horas < 10? `0${horas}`: horas}:{minutos < 10? `0${minutos}`: minutos}:{segundos < 10? `0${segundos}`: segundos}</h3>}
+                {msg}
             </div>            
         </>
         )
